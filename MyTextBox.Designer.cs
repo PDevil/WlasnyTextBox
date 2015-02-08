65e72b63f72f7b3d41aacfe6120b2209c42b9d18
PDevil <pawcio41@o2.pl>
2015-02-08 17:11:29 +0100

    Zachowanie kursora

--- MyTextBox.Designer.cs
+++ MyTextBox.Designer.cs
@@ -34,6 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.Name = "MyTextBox";
             this.Load += new System.EventHandler(this.MyTextBox_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.MyTextBox_Paint);

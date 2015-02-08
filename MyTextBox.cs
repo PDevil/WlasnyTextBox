f7bfa6a1fa1f1f24f74099105ccf1aec539ac40d
BuildTools <unconfigured@null.spigotmc.org>
2015-02-08 15:56:11 +0100

    User Control

---
+++ MyTextBox.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace MyTextBox
+{
+    public partial class MyTextBox : UserControl
+    {
+        public Color SelectedBackColor;
+        private int CurrentLine;
+        public SolidBrush BrushActiveLine;
+
+        [DllImport("user32.dll")]
+        static extern bool CreateCaret(IntPtr hWnd, IntPtr hBitmap, int nWidth, int nHeight);
+
+        public MyTextBox()
+        {
+            SelectedBackColor = Color.LightYellow;
+            CurrentLine = 0;
+            BrushActiveLine = new SolidBrush(SelectedBackColor);
+            InitializeComponent();
+        }
+
+        ~MyTextBox()
+        {
+            BrushActiveLine.Dispose();
+        }
+
+        private void MyTextBox_Paint(object sender, PaintEventArgs e)
+        {
+            int x = 0;
+            int y = CurrentLine * Font.Height;
+            e.Graphics.FillRectangle(BrushActiveLine, x, y, this.Size.Width, Font.Height);
+        }
+    }
+}
